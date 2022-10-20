b176f5906eff2ae6b765ce62718222638bceac0b
Durgante <108237050+GabrielDurgante@users.noreply.github.com>
2022-10-20 01:02:08 +0000

    print com +str

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # MEU PRIMEIRO PROJETO PYTHON!!!!!!
 
+'''
+Comentario em bloco
+'''
+
 print("Olá Mundo!!!")
 
 # guardar String (com aspas) e int (sem aspas)
 nome = "Gabriel Durgante"
 idade = 18
 
-print("meu nome é",nome,"e tenho",idade,"anos")
+print("meu nome é " +nome+ " e tenho "+str(idade),"anos") 
 
 
-
-'''
-Comentario em bloco
-'''
