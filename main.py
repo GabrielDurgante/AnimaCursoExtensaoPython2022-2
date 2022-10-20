2bda96158b04b7ac97ce6ebd95f95facd6ac0fed
Durgante <108237050+GabrielDurgante@users.noreply.github.com>
2022-10-20 01:18:08 +0000

    print com nome e idade com diferentes variações

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ print("Olá Mundo!!!")
 nome = "Gabriel Durgante"
 idade = 18
 
-print("meu nome é " +nome+ " e tenho "+str(idade),"anos") 
+# usando o +
+print("meu nome é " +nome+ " e tenho "+str(idade),"anos" "\n")
+
+#usando {} é necessario o f no inicio  
+print(f"Meu nome é {nome} e tenho {idade} anos \n")
+
+# usando .format
+print("Meu nome é {} e tenho {} anos ".format(nome,idade),"\n")
 
 
