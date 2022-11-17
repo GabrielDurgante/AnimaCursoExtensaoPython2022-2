062ba1286036c197817cde8dd771d0e0ee7400b1
Durgante <108237050+GabrielDurgante@users.noreply.github.com>
2022-11-17 01:21:13 +0000

    Exercicio pedido aaa

--- Aula4_2022_11_16_D.py
+++ Aula4_2022_11_16_D.py
@@ -6,6 +6,9 @@ nome = input("Informe o nome do Herói/Vilão: ")
 nome_civil = input("Informe o nome civil do Herói/Vilão: ")
 tipo_numerico = input("Tecle 1 para Herói ou 2 para Vilão: ")
 
+#consulta a tabela grupos e exibe a tabela 
+
+
 #Consulta para o valor maximo usado no banco
 sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
 cur.execute(sql)
@@ -17,6 +20,9 @@ else:
   tipo = "Vilã(o)"
 
   sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
+
+  #insert pessoas_grupo
+  #insert into pessoas_grupo(pessoa_id, grupo_id,) VALUES(x,x)
 
   cur.execute(sql)
   con.commit()
