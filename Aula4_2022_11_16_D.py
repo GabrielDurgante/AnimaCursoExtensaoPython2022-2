cb478af982cc7b59f77462e2cc20096d7b48a951
Durgante <108237050+GabrielDurgante@users.noreply.github.com>
2022-11-17 01:03:45 +0000

    Crie um sistema pra adicionar heróis/vilões

--- Aula4_2022_11_16_D.py
+++ Aula4_2022_11_16_D.py
@@ -2,5 +2,33 @@ import Aula4_2022_11_16_C as bd
 
 con, cur = bd.conectar()
 
+nome = input("Informe o nome do Herói/Vilão: ")
+nome_civil = input("Informe o nome civil do Herói/Vilão: ")
+tipo_numerico = input("Tecle 1 para Herói ou 2 para Vilão: ")
+
+#Consulta para o valor maximo usado no banco
+sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
+cur.execute(sql)
+pessoa_id = cur.fetchone()[0]
+
+if tipo_numerico == "1":
+  tipo = "Herói(na)"
+else:
+  tipo = "Vilã(o)"
+
+  sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
+
+  cur.execute(sql)
+  con.commit()
+  con.close()
+
+
+
+
+
+
+
+
+
 
 
